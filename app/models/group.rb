331e614f84a4fd787a67dc99d3ee61240b94d223
tGodson <tendongze95@gmail.com>
2020-09-14 21:56:42 +0100

    validate icon

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   mount_uploader :icon, AvatarUploader
-  validates_presence_of :icon
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   belongs_to :user
   has_many :transactions
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@
 
 Rails.application.routes.draw do
   root 'transactions#index'
-  get 'groups/index'
-  get 'sessions/index'
-  get 'users/new'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
