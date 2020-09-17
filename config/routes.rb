70cd54dad6f75bdd97e1ae7729a206cb193f4137
tGodson <tendongze95@gmail.com>
2020-09-17 14:03:15 +0100

    chand project title,add some negations to spec

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users
-  resources :groups
-  resources :transactions
+  resources :users, only: %i[create new show]
+  resources :groups, only: %i[index create new show]
+  resources :transactions, only: %i[index create new show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
